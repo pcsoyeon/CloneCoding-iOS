7a19c543f1759b0271de46b070e6ac5022130665
so yeon <alicesysy@swu.ac.kr>
2021-08-04 23:00:41 +0900

    [add] 앱스토어 프로젝트 추가

---
+++ AppStore-iOS/AppStore-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore-iOS
+//
+//  Created by soyeon on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
