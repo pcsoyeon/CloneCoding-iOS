95b824249f57ef9527a9c68c907a1a71e881ed17
so yeon <alicesysy@swu.ac.kr>
2021-08-05 12:18:54 +0900

    [chore] 폴더링 및 info.plist 수정

---
+++ AppStore-iOS/AppStore-iOS/Sources/TodayVC.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AppStore-iOS
+//
+//  Created by soyeon on 2021/08/04.
+//
+
+import UIKit
+
+class TodayVC: UIViewController {
+    
+    // MARK: - UI Components
+    
+
+    // MARK: - Local Variables
+    
+    
+    // MARK: - Life Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
+// MARK: - Custom Methods
+
+extension TodayVC {
+    
+}
+
